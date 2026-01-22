c0a454647ef9b70661465624dccdc28f7d6bfc5f
jayden <jaydenthung@hotmail.com>
2026-01-21 20:59:15 -0800

    update

--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Landing from './Landing.tsx'
 import Contact from './Contact.tsx'
